7a9e52c22f5d8478b7ccc547e09811cabbecf18d
veragusttavo3 <veragusttavo3@gmail.com>
2024-12-31 20:00:26 -0300

    saque js en un archivo aparte y lo llame desde el index

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,13 @@
- // JavaScript para mover el texto
- const h1 = document.getElementById('miTexto');
 
- h1.addEventListener('mousedown', function(event) {
-     let offsetX = event.clientX - h1.getBoundingClientRect().left;
-     let offsetY = event.clientY - h1.getBoundingClientRect().top;
+ // Inicializa el contador
+            let contador = localStorage.getItem('contador')? parseInt(localStorage.getItem('contador')) : 0;
+            
+            // Incrementa el contador
+            contador++;
+            
+            // Guarda el nuevo valor en localStorage
+            localStorage.setItem('contador', contador);
+            alert(contador);
+            // Muestra el contador en la página
+            document.getElementById('contador').textContent = contador;
  
-     function moverTexto(mouseMoveEvent) {
-         h1.style.left = mouseMoveEvent.clientX - offsetX + 'px';
-         h1.style.top = mouseMoveEvent.clientY - offsetY + 'px';
-     }
- 
-     function soltarTexto() {
-         document.removeEventListener('mousemove', moverTexto);
-         document.removeEventListener('mouseup', soltarTexto);
-     }
- 
-     document.addEventListener('mousemove', moverTexto);
-     document.addEventListener('mouseup', soltarTexto);
- });
-             // Inicializa el contador
-             let contador = localStorage.getItem('contador') ? parseInt(localStorage.getItem('contador')) : 0;
-             
-             // Incrementa el contador
-             contador++;
-             
-             // Guarda el nuevo valor en localStorage
-             localStorage.setItem('contador', contador);
-             
-             // Muestra el contador en la página
-             document.getElementById('contador').textContent = contador;
- 
-             
